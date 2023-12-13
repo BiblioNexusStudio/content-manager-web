cb4bb8a61008f570ab9b166da12c9b84c4263d49
Elisabeth <shakespearee04@gmail.com>
2023-12-13 15:25:15 -0600

    get rid of the PassageReference label, add logic for determining label, sort on passageRefs by startVerseId

--- src/lib/components/resources/BibleReferences.svelte
+++ src/lib/components/resources/BibleReferences.svelte
@@ -3,6 +3,20 @@
     import EmptyMessage from './EmptyMessage.svelte';
 
     export let bibleReferences: PassageReference[];
+
+    function generateVerseFromReference(reference: PassageReference): string {
+        let label = '';
+        if (reference.startBook === reference.endBook) {
+            if (reference.startChapter === reference.endChapter) {
+                label = `${reference.startBook} ${reference.startChapter}:${reference.startVerse}-${reference.endVerse}`;
+            } else {
+                label = `${reference.startBook} ${reference.startChapter}:${reference.startVerse}-${reference.endChapter}:${reference.endVerse}`;
+            }
+        } else {
+            label = `${reference.startBook} ${reference.startChapter}:${reference.startVerse} - ${reference.endBook} ${reference.endChapter}:${reference.endVerse}`;
+        }
+        return label;
+    }
 </script>
 
 <div class="flex min-h-[calc(26.5%-8px)] grow flex-col rounded-lg border border-base-300 bg-base-200">
@@ -12,7 +26,7 @@
             {#if bibleReferences.length > 0}
                 {#each bibleReferences as bibleReference}
                     <div class="mb-4 flex w-full justify-between">
-                        <p class="font-bold">{bibleReference.label}</p>
+                        <p class="font-bold">{generateVerseFromReference(bibleReference)}</p>
                     </div>
                 {/each}
             {:else}
--- src/routes/resources/[resourceContentId]/+page.svelte
+++ src/routes/resources/[resourceContentId]/+page.svelte
@@ -128,7 +128,11 @@
                     resourceContentStatuses={data.resourceContentStatuses}
                 />
                 <RelatedContent relatedContent={resourceContent.associatedResources} />
-                <BibleReferences bibleReferences={resourceContent.passageReferences} />
+                <BibleReferences
+                    bibleReferences={resourceContent.passageReferences.sort(
+                        (br1, br2) => br1.startVerseId - br2.startVerseId
+                    )}
+                />
             </div>
             <div class="flex max-h-full w-8/12 flex-col">
                 <Content {resourceContent} typeText={resourceContent.parentResourceName} />
