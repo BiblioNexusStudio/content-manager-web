d47cef863f5aa1fa45f9e5714a3261ab4f69f57f
Jesse Winston <jesse.winston@biblionexus.org>
2024-01-05 16:44:36 -0500

    Show draft / publish button

--- src/lib/components/resources/Content.svelte
+++ src/lib/components/resources/Content.svelte
@@ -2,22 +2,23 @@
     import Image from './content-components/Image.svelte';
     import Video from './content-components/Video.svelte';
     import TiptapContent from './content-components/TiptapContent.svelte';
-    import { MediaTypeEnum, type ResourceContent } from '$lib/types/resources';
+    import { MediaTypeEnum, type ResourceContentVersion } from '$lib/types/resources';
 
     export let mediaType: string;
-    export let resourceContent: ResourceContent;
+    export let resourceContentVersion: ResourceContentVersion;
     export let canEdit: boolean;
+    export let contentId: number;
 </script>
 
 <div class="relative flex h-full grow flex-col rounded-lg border border-base-300 bg-base-200">
     <div class="px-4 py-2 text-xl font-medium">Content</div>
     <div class="h-full overflow-y-scroll rounded-lg bg-white p-4">
         {#if mediaType === MediaTypeEnum.image}
-            <Image content={resourceContent.content} />
+            <Image content={resourceContentVersion.content} />
         {:else if mediaType === MediaTypeEnum.video}
-            <Video content={resourceContent.content} />
+            <Video content={resourceContentVersion.content} />
         {:else if mediaType === MediaTypeEnum.text}
-            <TiptapContent {canEdit} {resourceContent} />
+            <TiptapContent {canEdit} {resourceContentVersion} {contentId} />
         {/if}
     </div>
 </div>
