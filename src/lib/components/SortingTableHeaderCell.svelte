a2db667f08bc3dada4432158a2fb0d39c612a454
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2024-01-11 11:19:26 -0700

    add editor dashboard page

---
+++ src/lib/components/SortingTableHeaderCell.svelte
@@ -0,0 +1,25 @@
+﻿<script lang="ts">
+    export let text: string;
+    export let sortKey: string;
+    export let currentSort: string;
+
+    const determineCaret = (sortKey: string, currentSort: string): string => {
+        if (currentSort === sortKey) return '↑';
+        if (currentSort === '-' + sortKey) return '↓';
+        return '';
+    };
+
+    function toggleSort() {
+        const isAscending = currentSort === sortKey;
+        currentSort = isAscending ? '-' + sortKey : sortKey;
+    }
+</script>
+
+<th class="cursor-pointer select-none" on:click={toggleSort}>
+    {text}
+    {#if determineCaret(sortKey, currentSort) === ''}
+        <span class="invisible">↑</span>
+    {:else}
+        <span>{determineCaret(sortKey, currentSort)}</span>
+    {/if}
+</th>
---
+++ src/routes/EditorDashboard.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    import { unwrapStreamedData } from '$lib/utils/http-service';
+    import CenteredSpinner from '$lib/components/CenteredSpinner.svelte';
+    import { searchParameters, ssp } from '$lib/utils/sveltekit-search-params';
+    import type { ResourceAssignedToSelf } from './+page';
+    import SortingTableHeaderCell from '$lib/components/SortingTableHeaderCell.svelte';
+    import { createListSorter } from '$lib/utils/sorting';
+    import LinkedTableCell from '$lib/components/LinkedTableCell.svelte';
+
+    const SORT_KEYS = {
+        days: 'days',
+        wordCount: 'word-count',
+    };
+
+    const sortData = createListSorter<ResourceAssignedToSelf>({
+        [SORT_KEYS.days]: 'daysSinceAssignment',
+        [SORT_KEYS.wordCount]: 'wordCount',
+    });
+
+    export let data: PageData;
+
+    const searchParams = searchParameters({
+        sort: ssp.string(''),
+    });
+
+    $: resourceContentsPromise = unwrapStreamedData(data.editorDashboard!.resourceContent);
+
+    let scrollingDiv: HTMLDivElement | undefined;
+    $: $searchParams.sort && scrollingDiv && (scrollingDiv.scrollTop = 0);
+</script>
+
+{#await resourceContentsPromise}
+    <CenteredSpinner />
+{:then resourceContents}
+    <div class="flex max-h-screen flex-col overflow-y-hidden px-4">
+        <h1 class="pt-4 text-3xl">Editor Dashboard</h1>
+        <h2 class="pt-4 text-lg font-bold">Work Assigned to You ({resourceContents.length})</h2>
+        <div bind:this={scrollingDiv} class="my-4 max-h-full flex-grow overflow-y-scroll">
+            <table class="table table-pin-rows">
+                <thead>
+                    <tr class="bg-base-200">
+                        <th>Title</th>
+                        <th>Type</th>
+                        <SortingTableHeaderCell
+                            text="Days"
+                            sortKey={SORT_KEYS.days}
+                            bind:currentSort={$searchParams.sort}
+                        />
+                        <SortingTableHeaderCell
+                            text="Word Count"
+                            sortKey={SORT_KEYS.wordCount}
+                            bind:currentSort={$searchParams.sort}
+                        />
+                    </tr>
+                </thead>
+                <tbody>
+                    {#if resourceContents.length === 0}
+                        <tr>
+                            <td colspan="4" class="text-center">Your work is all done!</td>
+                        </tr>
+                    {:else}
+                        {#each sortData(resourceContents, $searchParams.sort) as resource}
+                            <tr>
+                                <LinkedTableCell href={`/resources/${resource.contentId}`}>
+                                    {resource.displayName}
+                                </LinkedTableCell>
+                                <LinkedTableCell href={`/resources/${resource.contentId}`}>
+                                    {resource.parentResourceName}
+                                </LinkedTableCell>
+                                <LinkedTableCell href={`/resources/${resource.contentId}`}>
+                                    {resource.daysSinceAssignment}
+                                </LinkedTableCell>
+                                <LinkedTableCell href={`/resources/${resource.contentId}`}>
+                                    {resource.wordCount}
+                                </LinkedTableCell>
+                            </tr>
+                        {/each}
+                    {/if}
+                </tbody>
+            </table>
+        </div>
+    </div>
+{/await}
