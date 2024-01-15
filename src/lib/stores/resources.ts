815b8ec89291b34ad171ef502aaa73e603ab7adb
Elisabeth <shakespearee04@gmail.com>
2024-01-15 09:32:02 -0600

    default page count to 100

--- src/lib/stores/resources.ts
+++ src/lib/stores/resources.ts
@@ -3,7 +3,7 @@ import { browser } from '$app/environment';
 
 export const languageId = writable<number>(1);
 
-const storedPerPage = (browser && localStorage.getItem('resourcesPerPage')) || 10;
+const storedPerPage = (browser && localStorage.getItem('resourcesPerPage')) || 100;
 
 export const resourcesPerPage = writable<number>(+storedPerPage);
 
