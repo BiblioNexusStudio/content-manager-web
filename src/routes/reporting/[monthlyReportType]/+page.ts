95facba9a718e01abfc677b8ef1ea34e281a7dcb
Elisabeth <shakespearee04@gmail.com>
2024-01-25 13:33:50 -0600

    rename the aquiferization reporting to where it is parameterized, and will hit {reportType} endpoint on backend and supply report; cleanup eng.json because commit hook was complaining

--- src/routes/reporting/[monthlyReportType]/+page.ts
+++ src/routes/reporting/[monthlyReportType]/+page.ts
@@ -2,11 +2,14 @@ import type { PageLoad } from './$types';
 import { fetchJsonStreamingFromApi, type StreamedData } from '$lib/utils/http-service';
 import type { MonthlyStartsAndCompletions } from '$lib/types/reporting';
 
-export const load: PageLoad = async ({ fetch }) => {
+export const load: PageLoad = async ({ params, fetch }) => {
     const report = fetchJsonStreamingFromApi(
-        '/reports/monthly/aquiferization',
+        `/reports/${params.monthlyReportType}/monthly`,
         {},
         fetch
     ) as StreamedData<MonthlyStartsAndCompletions>;
-    return { report };
+    return {
+        reportType: params.monthlyReportType,
+        report,
+    };
 };
