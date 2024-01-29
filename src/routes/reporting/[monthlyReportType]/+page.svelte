3b265608f65421700adef63685cd09a028b26db8
elisabeth-shakespeare <153534973+elisabeth-shakespeare@users.noreply.github.com>
2024-01-29 10:16:08 -0600

    Bib 329 reporting updates (#89)
    rename MonthlyChart, add a store to keep up with whether to show the side menu, styling changes

--- src/routes/reporting/[monthlyReportType]/+page.svelte
+++ src/routes/reporting/[monthlyReportType]/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
     import type { PageData } from './$types';
-    import { _ as translate } from 'svelte-i18n';
     import { unwrapStreamedData } from '$lib/utils/http-service';
     import CenteredSpinner from '$lib/components/CenteredSpinner.svelte';
-    import MonthlyAquiferizationsChart from '$lib/charts/MonthlyAquiferizationsChart.svelte';
+    import { showSideBar } from '$lib/stores/app';
+    import MonthlyStartsAndCompletionsChart from '$lib/charts/MonthlyStartsAndCompletionsChart.svelte';
 
     export let data: PageData;
+    $showSideBar = false;
     $: reportType = data.reportType;
 
     $: reportPromise = unwrapStreamedData(data.report);
@@ -15,13 +16,12 @@
     <CenteredSpinner />
 {:then report}
     <div class="mx-4 grid grid-cols-2">
-        <div class="col-span-2 mt-4 text-3xl">{$translate('page.dashboard.header.value')}</div>
-        <div class="col-span-2 mb-6 mt-4 text-lg font-bold">
+        <div class="col-span-2 mb-6 mt-4 text-3xl">
             {reportType.charAt(0).toUpperCase() + reportType.slice(1)} History
         </div>
 
         <div class="me-10 ms-5">
-            <MonthlyAquiferizationsChart completesByMonth={report.completions} startsByMonth={report.starts} />
+            <MonthlyStartsAndCompletionsChart completesByMonth={report.completions} startsByMonth={report.starts} />
         </div>
     </div>
 {/await}
