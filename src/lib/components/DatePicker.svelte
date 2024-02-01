ec05e5570107596b7e2276c80ae60fdea5a1e975
Carlos Morales <carlos.morales@biblionexus.org>
2024-02-01 16:02:58 -0500

    madeRequestedPrChanges

--- src/lib/components/DatePicker.svelte
+++ src/lib/components/DatePicker.svelte
@@ -1,21 +1,7 @@
 <script lang="ts">
-    import { createEventDispatcher } from 'svelte';
-
     export let date: string;
-
-    const dispatch = createEventDispatcher();
-
-    function handleDateChange(event: Event) {
-        date = (event.target as HTMLInputElement).value;
-        dispatch('dateChange', date);
-    }
 </script>
 
 <div class="flex h-full w-auto items-center justify-center">
-    <input
-        type="date"
-        bind:value={date}
-        on:change={handleDateChange}
-        class="h-auto w-full rounded-md border px-4 py-2"
-    />
+    <input type="date" bind:value={date} class="h-auto w-full rounded-md border px-4 py-2" />
 </div>
