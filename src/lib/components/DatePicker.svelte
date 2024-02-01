274481b6f4453ac971bc2ce4f5a98ecc4ba7980e
Carlos Morales <carlos.morales@biblionexus.org>
2024-02-01 14:12:17 -0500

    addingReusableDatePickerComponent

---
+++ src/lib/components/DatePicker.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+
+    export let date: string;
+
+    const dispatch = createEventDispatcher();
+
+    function handleDateChange(event: Event) {
+        date = (event.target as HTMLInputElement).value;
+        dispatch('dateChange', date);
+    }
+</script>
+
+<div class="flex h-full w-auto items-center justify-center">
+    <input
+        type="date"
+        bind:value={date}
+        on:change={handleDateChange}
+        class="h-auto w-full rounded-md border px-4 py-2"
+    />
+</div>
