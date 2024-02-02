ccd87481bb826787fc40b53f3ba386b232c219c4
Carlos Morales <carlos.rey.morales@gmail.com>
2024-02-02 15:10:55 -0500

    addingReusableDatePickerComponent (#95)
    * addingReusableDatePickerComponent
    
    * madeRequestedPrChanges

---
+++ src/lib/components/DatePicker.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    export let date: string;
+</script>
+
+<div class="flex h-full w-auto items-center justify-center">
+    <input type="date" bind:value={date} class="h-auto w-full rounded-md border px-4 py-2" />
+</div>
