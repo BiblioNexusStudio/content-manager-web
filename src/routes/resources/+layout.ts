eec8857dc90a40f15cf9b71b320f5ec1a8308662
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2024-02-26 12:22:29 -0700

    make improvements to resources page filtering (#140)
    - Uses checkboxes instead of dropdown for selecting published vs unpublished
    - Improves placeholder text for chapter selection
    - Prevents invalid chapter selection from being applied
    - Uses streaming more instead of a hard await to make page feel snappier
    - Fix regression with not starting back at page 1 when changing filters

---
+++ src/routes/resources/+layout.ts
@@ -0,0 +1,15 @@
+import { fetchJsonStreamingFromApi } from '$lib/utils/http-service';
+import type { Bible } from '$lib/types/base';
+import type { LayoutLoad } from './$types';
+
+export const ssr = false;
+
+export const load: LayoutLoad = async ({ fetch, parent }) => {
+    const { languages } = await parent();
+
+    const englishLanguageId = languages?.find((l) => l.iso6393Code === 'eng')?.id;
+
+    return {
+        bibles: fetchJsonStreamingFromApi<Bible[]>(`/bibles/language/${englishLanguageId}`, {}, fetch),
+    };
+};
