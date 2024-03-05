d55f12a0730b3d813e64ef871764a558efa24654
elisabeth-shakespeare <153534973+elisabeth-shakespeare@users.noreply.github.com>
2024-03-05 08:24:25 -0600

    Bib 384 there are not enough distinct colors in translated resources (#157)
    * on a fresh load, get a mapping from language to color; sort languages based on data, and assign to colors inthat order. have 16 static colors for assignment

--- src/lib/utils/color-gen.ts
+++ src/lib/utils/color-gen.ts
@@ -1,26 +1,42 @@
-export const generateColors = (colorAmount: number) => {
+export const generateColors = (dataMap: { label: string; data: number[] }[]) => {
     // tetradic color hues
-    const colorHues = [197, 287, 17, 107];
-    const min = 32;
-    const maxLight = 82;
-    let lightness = min;
-    let hueidx = 0;
-    let hue = colorHues[hueidx];
-    const colorJump = Math.floor((maxLight - min) / (colorAmount / colorHues.length));
-    const colors: string[] = [];
 
-    for (let i = 0; i < colorAmount; i++) {
-        colors[i] = `hsl(${hue}, 70%, ${lightness}%)`;
-        hue = colorHues[++hueidx];
-        if (hueidx % colorHues.length == 0) {
-            hueidx = 0;
-            hue = colorHues[hueidx];
-            if (lightness < maxLight - colorJump) {
-                lightness += colorJump;
-            } else {
-                lightness = min;
-            }
+    const colors = [
+        '#00A3E0', //primary
+        '#344054',
+        '#e18b47', // orange
+        '#edbf84',
+        '#8671b9', // purple
+        '#beb4d3',
+        '#7f5b50', // brown
+        '#b99f97',
+        '#c987c1', //pink
+        '#e6bdc9',
+        '#669838', // green
+        '#b3d78f',
+        '#b74a3d', // red
+        '#e4a19b',
+        '#7f7f7f', // gray
+        '#cccccc',
+    ];
+
+    const labeledColors = [] as { color: string; language: string }[];
+    dataMap.sort((a, b) => {
+        if (a.data.length > 0 && b.data.length > 0) {
+            const sumA = a.data.reduce((total, current) => (total += current));
+            const sumB = b.data.reduce((total, current) => (total += current));
+            return sumB - sumA;
         }
+
+        return 0;
+    });
+
+    for (let i = 0; i < dataMap.length; i++) {
+        labeledColors.push({
+            color: colors[i]!,
+            language: dataMap[i]!.label,
+        });
     }
-    return colors;
+
+    return labeledColors;
 };
