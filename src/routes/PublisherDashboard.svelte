d0f5231aa203328c8c9e059a27c8d4a8e4123fb3
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2024-03-08 08:28:01 -0700

    fix sort order on publisher my projects tab

--- src/routes/PublisherDashboard.svelte
+++ src/routes/PublisherDashboard.svelte
@@ -81,7 +81,7 @@
 {:then [assignedContents, reviewPendingContents, assignedProjects, reportingSummary]}
     <div class="flex max-h-screen flex-col overflow-y-hidden px-4">
         <h1 class="pt-4 text-3xl">Publisher Dashboard</h1>
-        <div role="tablist" class="tabs tabs-bordered w-fit pt-4">
+        <div role="tablist" class="tabs-bordered tabs w-fit pt-4">
             <button
                 on:click={() => ($searchParams.tab = Tab.myWork)}
                 role="tab"
