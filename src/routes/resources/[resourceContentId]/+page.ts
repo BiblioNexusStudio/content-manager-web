8cafceb93477fd91757eb2218d6b0705b9dcc7ce
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2024-03-14 11:57:41 -0600

    ensure auth is initialized before any calls are made (#172)

--- src/routes/resources/[resourceContentId]/+page.ts
+++ src/routes/resources/[resourceContentId]/+page.ts
@@ -2,7 +2,9 @@ import type { PageLoad } from './$types';
 import { getFromApiWithoutBlocking } from '$lib/utils/http-service';
 import type { ResourceContent } from '$lib/types/resources';
 
-export const load: PageLoad = async ({ params, fetch }) => {
+export const load: PageLoad = async ({ parent, params, fetch }) => {
+    await parent();
+
     return {
         resourceContentId: params.resourceContentId,
         resourceContent: getFromApiWithoutBlocking<ResourceContent>(
