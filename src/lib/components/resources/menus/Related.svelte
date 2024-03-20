a51d5beffebe7b16ae3f5543087c10647e878ed3
Carlos Morales <carlos.rey.morales@gmail.com>
2024-03-20 16:45:41 -0400

    uiBugfixes (#181)

--- src/lib/components/resources/menus/Related.svelte
+++ src/lib/components/resources/menus/Related.svelte
@@ -25,9 +25,9 @@
 <div class="dropdown ms-1">
     <button class="btn btn-ghost ms-2 whitespace-nowrap px-1 hover:bg-[#e6f6fc]"> Related Content </button>
     <div class="menu dropdown-content z-[1] mt-4 max-h-72 w-auto rounded-box border bg-base-100 px-8 pt-4 shadow">
-        <div class="flex flex-col">
+        <div class="flex flex-col overflow-y-scroll">
             {#each relatedContent as resource, i (i)}
-                <div class="mb-4 flex">
+                <div class="mb-4 me-2 flex">
                     <div class="flex items-center">
                         <Icon data={getIcon(resource.mediaTypes[0])} scale={3} />
                     </div>
