965f913ecfbb4bd68a74fe4478acfd68c95c8c3a
Kyle Grinstead <kyle.grinstead@biblionexus.org>
2024-03-26 12:25:45 -0600

    fix tooltip so it only shows when target element is hovered

--- src/lib/components/Tooltip.svelte
+++ src/lib/components/Tooltip.svelte
@@ -3,14 +3,16 @@
     export let position: { top?: string; left?: string; bottom?: string; right?: string };
 </script>
 
-<div class="tooltip-container group relative flex items-center">
-    <slot />
+<div class="tooltip-container relative flex items-center">
+    <div class="peer">
+        <slot />
+    </div>
 
     <div
         style="top: {position.top ?? 'none'}; left: {position.left ?? 'none'}; bottom: {position.bottom ??
             'none'}; right:
         {position.right ?? 'none'};"
-        class="tooltip absolute whitespace-nowrap rounded-xl border-2 px-2 text-sm font-bold opacity-0 transition group-hover:opacity-100
+        class="tooltip absolute whitespace-nowrap rounded-xl border-2 px-2 text-sm font-bold opacity-0 transition peer-hover:opacity-100
         {$$props.class}"
     >
         {text}
