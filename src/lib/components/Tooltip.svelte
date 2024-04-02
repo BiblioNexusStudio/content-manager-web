f7febc6de9166a131cee119460b65e1f00e5db79
Jesse Winston <jesse.winston@biblionexus.org>
2024-04-02 12:35:16 -0400

    Remove unneeded extra classes on tooltip

--- src/lib/components/Tooltip.svelte
+++ src/lib/components/Tooltip.svelte
@@ -12,7 +12,7 @@
         style="top: {position.top ?? 'none'}; left: {position.left ?? 'none'}; bottom: {position.bottom ??
             'none'}; right:
         {position.right ?? 'none'};"
-        class="tooltip absolute hidden whitespace-nowrap rounded-xl border-2 bg-white px-2 text-sm font-bold opacity-0 transition peer-hover:flex peer-hover:opacity-100
+        class="tooltip absolute hidden whitespace-nowrap rounded-xl border-2 bg-white px-2 text-sm font-bold transition peer-hover:flex
         {$$props.class}"
     >
         {text}
