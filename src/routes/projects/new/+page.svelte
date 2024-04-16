bcc5ae9a6bd09df7f4e91d0b1b3e4d33b75bb1f3
Jesse Winston <138067194+jwinston-bn@users.noreply.github.com>
2024-04-16 09:33:53 -0400

    More compact inputs area on project creation (#215)
    Before:
    
    
    ![image](https://github.com/BiblioNexusStudio/content-manager-web/assets/138067194/89404aa2-d007-43a4-949c-15eb15033841)
    
    After:
    
    
    ![image](https://github.com/BiblioNexusStudio/content-manager-web/assets/138067194/080c10ce-fa70-4f34-a442-72afe965147f)

--- src/routes/projects/new/+page.svelte
+++ src/routes/projects/new/+page.svelte
@@ -77,86 +77,91 @@
         <div class="text-3xl">Create Project</div>
     </div>
     <div class="mb-8 flex flex-col">
-        <div class="pb-2 text-lg font-bold">Basic Information</div>
-        <div class="flex flex-row items-center border-b border-t p-2">
-            <div class="text-md">Title <span class="text-error">*</span></div>
-            <div class="flex-grow"></div>
-            <input class="input input-bordered w-full max-w-[50%]" bind:value={title} />
-        </div>
-        <div class="flex flex-row items-center border-b p-2">
-            <div class="text-md">Language <span class="text-error">*</span></div>
-            <div class="flex-grow"></div>
-            <Select
-                class="select select-bordered w-full max-w-[50%]"
-                disabled={selectedResourceIds.length > 0}
-                options={[
-                    { value: null, label: 'Select Language' },
-                    ...(languages || []).map((l) => ({
-                        value: l.id,
-                        label: l.iso6393Code === 'eng' ? 'English (Aquiferize)' : l.englishDisplay,
-                    })),
-                ]}
-                isNumber={true}
-                bind:value={languageId}
-            />
-        </div>
-        <div class="flex flex-row items-center border-b p-2">
-            <div class="text-md">Project Manager <span class="text-error">*</span></div>
-            <div class="flex-grow"></div>
-            <Select
-                class="select select-bordered w-full max-w-[50%]"
-                options={[
-                    { value: null, label: 'Select User' },
-                    ...(users || [])
-                        .filter((u) => u.role === UserRole.Publisher)
-                        .map((u) => ({ value: u.id, label: u.name })),
-                ]}
-                isNumber={true}
-                bind:value={projectManagerUserId}
-            />
-        </div>
-        <div class="flex flex-row items-center border-b p-2">
-            <div class="text-md">Company <span class="text-error">*</span></div>
-            <div class="flex-grow"></div>
-            <Select
-                class="select select-bordered w-full max-w-[50%]"
-                options={[
-                    { value: null, label: 'Select Company' },
-                    ...(companies || []).filter((c) => c.name !== 'N/A').map((c) => ({ value: c.id, label: c.name })),
-                ]}
-                isNumber={true}
-                bind:value={companyId}
-            />
-        </div>
-        <div class="flex flex-row items-center border-b p-2">
-            <div class="text-md">Platform <span class="text-error">*</span></div>
-            <div class="flex-grow"></div>
-            <Select
-                class="select select-bordered w-full max-w-[50%]"
-                options={[
-                    { value: null, label: 'Select Platform' },
-                    ...(projectPlatforms || []).map((c) => ({ value: c.id, label: c.name })),
-                ]}
-                isNumber={true}
-                bind:value={platformId}
-            />
-        </div>
-        <div class="border-b p-2">
-            <div class="{!requiresCompanyLead && 'contrast-0'} flex flex-row items-center">
-                <div class="text-md">Company Lead <span class="text-error">*</span></div>
+        <div class="w-full pb-2 text-lg font-bold">Basic Information</div>
+        <div class="grid grid-cols-2 gap-4">
+            <div class="flex flex-row items-center border-b p-2">
+                <div class="text-md">Title <span class="text-error">*</span></div>
+                <div class="flex-grow"></div>
+                <input class="input input-bordered input-sm w-full max-w-[50%]" bind:value={title} />
+            </div>
+            <div class="flex flex-row items-center border-b p-2">
+                <div class="text-md">Language <span class="text-error">*</span></div>
                 <div class="flex-grow"></div>
                 <Select
-                    disabled={!requiresCompanyLead}
-                    class="select select-bordered w-full max-w-[50%] {!requiresCompanyLead && '!bg-transparent'}"
+                    class="select select-bordered select-sm w-full max-w-[50%]"
+                    disabled={selectedResourceIds.length > 0}
+                    options={[
+                        { value: null, label: 'Select Language' },
+                        ...(languages || []).map((l) => ({
+                            value: l.id,
+                            label: l.iso6393Code === 'eng' ? 'English (Aquiferize)' : l.englishDisplay,
+                        })),
+                    ]}
+                    isNumber={true}
+                    bind:value={languageId}
+                />
+            </div>
+            <div class="flex flex-row items-center border-b p-2">
+                <div class="text-md">Project Manager <span class="text-error">*</span></div>
+                <div class="flex-grow"></div>
+                <Select
+                    class="select select-bordered select-sm w-full max-w-[50%]"
                     options={[
                         { value: null, label: 'Select User' },
                         ...(users || [])
-                            .filter((u) => u.role === UserRole.Publisher || u.role === UserRole.Manager)
+                            .filter((u) => u.role === UserRole.Publisher)
+                            .map((u) => ({ value: u.id, label: u.name })),
+                    ]}
+                    isNumber={true}
+                    bind:value={projectManagerUserId}
+                />
+            </div>
+            <div class="flex flex-row items-center border-b p-2">
+                <div class="text-md">Company <span class="text-error">*</span></div>
+                <div class="flex-grow"></div>
+                <Select
+                    class="select select-bordered select-sm w-full max-w-[50%]"
+                    options={[
+                        { value: null, label: 'Select Company' },
+                        ...(companies || [])
+                            .filter((c) => c.name !== 'N/A')
                             .map((c) => ({ value: c.id, label: c.name })),
                     ]}
                     isNumber={true}
-                    bind:value={companyLeadUserId}
+                    bind:value={companyId}
                 />
+            </div>
+            <div class="flex flex-row items-center border-b p-2">
+                <div class="text-md">Platform <span class="text-error">*</span></div>
+                <div class="flex-grow"></div>
+                <Select
+                    class="select select-bordered select-sm w-full max-w-[50%]"
+                    options={[
+                        { value: null, label: 'Select Platform' },
+                        ...(projectPlatforms || []).map((c) => ({ value: c.id, label: c.name })),
+                    ]}
+                    isNumber={true}
+                    bind:value={platformId}
+                />
+            </div>
+            <div class="border-b p-2">
+                <div class="{!requiresCompanyLead && 'contrast-0'} flex flex-row items-center">
+                    <div class="text-md">Company Lead <span class="text-error">*</span></div>
+                    <div class="flex-grow"></div>
+                    <Select
+                        disabled={!requiresCompanyLead}
+                        class="select select-bordered select-sm w-full max-w-[50%] {!requiresCompanyLead &&
+                            '!bg-transparent'}"
+                        options={[
+                            { value: null, label: 'Select User' },
+                            ...(users || [])
+                                .filter((u) => u.role === UserRole.Publisher || u.role === UserRole.Manager)
+                                .map((c) => ({ value: c.id, label: c.name })),
+                        ]}
+                        isNumber={true}
+                        bind:value={companyLeadUserId}
+                    />
+                </div>
             </div>
         </div>
     </div>
