2943cc959dd0dd1d48c416060a8fe54797d34c5a
Jesse Winston <jesse.winston@biblionexus.org>
2024-07-18 11:36:32 -0400

    Stupid linter

--- src/routes/(dashboard)/ManagerDashboard.svelte
+++ src/routes/(dashboard)/ManagerDashboard.svelte
@@ -216,7 +216,7 @@
     <div class="flex max-h-screen flex-col overflow-y-hidden px-4">
         <h1 class="pt-4 text-3xl">Manager Dashboard</h1>
         <div class="flex flex-row items-center pt-4">
-            <div role="tablist" class="tabs tabs-bordered w-fit">
+            <div role="tablist" class="tabs-bordered tabs w-fit">
                 <button
                     on:click={() => switchTabs(Tab.myWork)}
                     role="tab"
