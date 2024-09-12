d0e65132c698c173596f84579d350fea2d6ac12a
Kasey Wright <kasey.wright@biblionexus.org>
2024-09-12 10:51:13 -0400

    Add tool tip to the scoll lock button (#406)

--- src/lib/components/editor/ScrollSyncLockToggle.svelte
+++ src/lib/components/editor/ScrollSyncLockToggle.svelte
@@ -2,16 +2,25 @@
     import LockClosedIcon from '$lib/icons/LockClosedIcon.svelte';
     import LockOpenIcon from '$lib/icons/LockOpenIcon.svelte';
     import { isScrollSyncEnabled } from '$lib/stores/scrollSync';
+    import Tooltip from '../Tooltip.svelte';
+
+    $: tooltipStr = $isScrollSyncEnabled ? 'Disable' : 'Enable';
 </script>
 
-<button
-    on:click={() => ($isScrollSyncEnabled = !$isScrollSyncEnabled)}
-    class="btn h-auto min-h-0 p-1"
-    class:btn-primary={$isScrollSyncEnabled}
+<Tooltip
+    position={{ right: '2rem', top: '0.25rem' }}
+    class="flex border-primary align-middle text-primary"
+    text="{tooltipStr} Scroll Lock"
 >
-    {#if $isScrollSyncEnabled}
-        <LockClosedIcon />
-    {:else}
-        <LockOpenIcon />
-    {/if}
-</button>
+    <button
+        on:click={() => ($isScrollSyncEnabled = !$isScrollSyncEnabled)}
+        class="btn mt-1 h-auto min-h-0 p-1"
+        class:btn-primary={$isScrollSyncEnabled}
+    >
+        {#if $isScrollSyncEnabled}
+            <LockClosedIcon />
+        {:else}
+            <LockOpenIcon />
+        {/if}
+    </button>
+</Tooltip>
