8b7ae6a39daa48a95d73e3c44a2c3dc2dc6cd906
kaseywright <kasey.wright@biblionexus.org>
2024-10-03 10:59:38 -0400

    Add overflow-x-auto to community dashboard to avoid super wide filter controls

--- src/routes/(dashboard)/CommunityReviewerDashboard.svelte
+++ src/routes/(dashboard)/CommunityReviewerDashboard.svelte
@@ -162,7 +162,7 @@
                 >
             </div>
         </div>
-        <div class="flex gap-4">
+        <div class="flex gap-4 overflow-x-auto">
             {#if $searchParams.tab === Tab.resources}
                 <input
                     class="input input-bordered max-w-xs focus:outline-none"
