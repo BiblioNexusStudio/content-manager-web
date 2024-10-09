8ee1edb8fa347fe4e93a2207d9b0ab881d65971e
kaseywright <kasey.wright@biblionexus.org>
2024-10-09 10:00:12 -0400

    Add ContentEditorSwapButton component Add ArrowsLetRight svg component

---
+++ src/lib/components/editor/ContentEditorSwapButton.svelte
@@ -0,0 +1,15 @@
+<script>
+    import ArrowsLetRight from '$lib/icons/ArrowsLetRight.svelte';
+    import Tooltip from '../Tooltip.svelte';
+    import { isLeftToRight } from '$lib/stores/resourceEditor';
+</script>
+
+<Tooltip
+    position={{ right: '2rem', top: '0.25rem' }}
+    class="flex border-primary align-middle text-primary"
+    text="Swap Editor Panes"
+>
+    <button on:click={() => ($isLeftToRight = !$isLeftToRight)} class="btn mt-1 h-auto min-h-0 p-1">
+        <ArrowsLetRight />
+    </button>
+</Tooltip>
