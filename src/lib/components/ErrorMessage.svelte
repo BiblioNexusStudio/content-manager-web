3fdc43d0df3f24b0befae971499c8c1dfb04af71
Carlos Morales <carlos.morales@biblionexus.org>
2024-10-14 11:33:40 -0400

    addedRateLimitMessageToErrorPage

--- src/lib/components/ErrorMessage.svelte
+++ src/lib/components/ErrorMessage.svelte
@@ -16,13 +16,28 @@
 
     let didNotRedirect = false;
 
-    const buttonText = gotoPath === '/' ? 'Go Home' : `Go To ${gotoPath.replace('/', '')}`;
+    let buttonText = gotoPath === '/' ? 'Go Home' : `Go To ${gotoPath.replace('/', '')}`;
 
     const error = uncastError as Error | FetchError | ApiError | TokenMissingError | AuthUninitializedError;
+
+    let errorMessage = 'An error occurred';
+
+    function handleButtonClick() {
+        if (isApiErrorWithStatus(error, 429)) {
+            // SvelteKit's goto function does not react to window.location.pathname;
+            location.reload();
+        }
+
+        goto(gotoPath);
+    }
 
     onMount(() => {
         if (isApiErrorWithStatus(error, 404) || $page.status === 404) {
             goto(gotoPath);
+        } else if (isApiErrorWithStatus(error, 429)) {
+            errorMessage = 'Rate limit exceeded.';
+            buttonText = 'Refresh';
+            didNotRedirect = true;
         } else {
             didNotRedirect = true;
         }
@@ -32,8 +47,8 @@
 {#if didNotRedirect}
     <div class="flex h-[calc(100vh-39px)] w-full items-center justify-center">
         <div class="flex grow flex-col items-center">
-            <h1 class="text-4xl">An error occurred</h1>
-            <button on:click={() => goto(gotoPath)} class="btn btn-primary mt-4 capitalize">{buttonText}</button>
+            <h1 class="text-4xl">{errorMessage}</h1>
+            <button on:click={handleButtonClick} class="btn btn-primary mt-4 capitalize">{buttonText}</button>
         </div>
     </div>
 {:else}
