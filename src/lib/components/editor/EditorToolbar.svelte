7732be21e4e208e8c0281e704a00f30e8698be9b
Elisabeth <shakespearee04@gmail.com>
2024-10-21 08:51:27 -0400

    lint

--- src/lib/components/editor/EditorToolbar.svelte
+++ src/lib/components/editor/EditorToolbar.svelte
@@ -198,7 +198,7 @@
     });
 </script>
 
-<div bind:this={outerDiv} class="min-h-6 flex items-center justify-between">
+<div bind:this={outerDiv} class="flex min-h-6 items-center justify-between">
     {#if editor}
         {@const commentOptions = getCommentOptions(editor)}
         {@const commentDisabled = commentOptions.disabled || $isPageTransacting}
