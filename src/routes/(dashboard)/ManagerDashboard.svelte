6389865986079b9c08bc383998b157c3b30e7797
kaseywright <kasey.wright@biblionexus.org>
2025-02-06 07:03:07 -0500

    Add audio indicator to Manager dashboard tabs

--- src/routes/(dashboard)/ManagerDashboard.svelte
+++ src/routes/(dashboard)/ManagerDashboard.svelte
@@ -24,6 +24,8 @@
         userWordCountColumns,
     } from './manager-dashboard-columns';
     import { untrack } from 'svelte';
+    import { Icon } from 'svelte-awesome';
+    import volumeUp from 'svelte-awesome/icons/volumeUp';
 
     interface Props {
         data: PageData;
@@ -455,6 +457,12 @@
                     >
                 {:else if itemKey === 'lastAssignedUser'}
                     <LinkedTableCell {href}>{item[itemKey]?.name ?? ''}</LinkedTableCell>
+                {:else if itemKey === 'hasAudio'}
+                    <TableCell>
+                        {#if item.hasAudio}
+                            <Icon data={volumeUp} class="h-4 w-4" />
+                        {/if}
+                    </TableCell>
                 {:else if href !== undefined && itemKey}
                     <LinkedTableCell {href}>{item[itemKey] ?? ''}</LinkedTableCell>
                 {:else if itemKey}
@@ -485,6 +493,12 @@
                         <LinkedTableCell {href} class={(item[itemKey] ?? 0) < 0 ? 'text-error' : ''}
                             >{item[itemKey] ?? ''}</LinkedTableCell
                         >
+                    {:else if itemKey === 'hasAudio'}
+                        <TableCell>
+                            {#if item.hasAudio}
+                                <Icon data={volumeUp} class="h-4 w-4" />
+                            {/if}
+                        </TableCell>
                     {:else if href !== undefined && itemKey}
                         <LinkedTableCell {href}>{item[itemKey] ?? ''}</LinkedTableCell>
                     {:else if itemKey}
@@ -533,6 +547,12 @@
                         >
                     {:else if itemKey === 'lastAssignedUser'}
                         <LinkedTableCell {href}>{item[itemKey]?.name ?? ''}</LinkedTableCell>
+                    {:else if itemKey === 'hasAudio'}
+                        <TableCell>
+                            {#if item.hasAudio}
+                                <Icon data={volumeUp} class="h-4 w-4" />
+                            {/if}
+                        </TableCell>
                     {:else if href !== undefined && itemKey}
                         <LinkedTableCell {href}>{item[itemKey] ?? ''}</LinkedTableCell>
                     {:else if itemKey}
