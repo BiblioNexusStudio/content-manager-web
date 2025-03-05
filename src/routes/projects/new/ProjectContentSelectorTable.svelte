04db1d0ceb1062202b81a8977a8ccbf5836ef5fd
Jesse Winston <jesse.winston@biblionexus.org>
2025-03-05 12:10:44 -0500

    lint

--- src/routes/projects/new/ProjectContentSelectorTable.svelte
+++ src/routes/projects/new/ProjectContentSelectorTable.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import CenteredSpinner from '$lib/components/CenteredSpinner.svelte';
-    import type {ResourceContentForSelection} from './types';
+    import type { ResourceContentForSelection } from './types';
 
     interface Props {
         allContent: ResourceContentForSelection[];
@@ -10,20 +10,13 @@
         hasSearched?: boolean;
     }
 
-    let {
-        allContent,
-        selectedIds = $bindable(),
-        isLoading = false,
-        hasSearched = false,
-    }: Props = $props();
+    let { allContent, selectedIds = $bindable(), isLoading = false, hasSearched = false }: Props = $props();
 
     let allSelected = $derived(
         allContent.length > 0 && allContent.every((c) => selectedIds.some((id) => id === c.resourceId))
     );
 
-    let selectedContent = $derived(
-        allContent.filter((c) => selectedIds.includes(c.resourceId))
-    );
+    let selectedContent = $derived(allContent.filter((c) => selectedIds.includes(c.resourceId)));
 
     function selectOrDeselectAll() {
         if (allSelected) {
@@ -46,59 +39,59 @@
 
 <table class="table table-pin-rows">
     <thead>
-    <tr class="bg-base-200">
-        <th class="w-[1px]"
-        ><input
-                checked={allSelected}
-                class="checkbox"
-                disabled={allContent.length === 0}
-                onchange={selectOrDeselectAll}
-                type="checkbox"
-        /></th
-        >
-        <th>Title ({allContent.length} total)
-            {#if selectedIds.length > 0}
-                ({selectedIds.length} selected)
-            {/if}
-        </th>
-        <th class="w-[1px]"
-        >Words
-            ({allContent.reduce((sum, content) => sum + content.wordCount, 0).toLocaleString()} total)
-            {#if selectedIds.length > 0}
-                ({selectedContent.reduce((sum, content) => sum + content.wordCount, 0).toLocaleString()} selected)
-            {/if}
-        </th>
-    </tr>
+        <tr class="bg-base-200">
+            <th class="w-[1px]"
+                ><input
+                    checked={allSelected}
+                    class="checkbox"
+                    disabled={allContent.length === 0}
+                    onchange={selectOrDeselectAll}
+                    type="checkbox"
+                /></th
+            >
+            <th
+                >Title ({allContent.length} total)
+                {#if selectedIds.length > 0}
+                    ({selectedIds.length} selected)
+                {/if}
+            </th>
+            <th class="w-[1px]"
+                >Words ({allContent.reduce((sum, content) => sum + content.wordCount, 0).toLocaleString()} total)
+                {#if selectedIds.length > 0}
+                    ({selectedContent.reduce((sum, content) => sum + content.wordCount, 0).toLocaleString()} selected)
+                {/if}
+            </th>
+        </tr>
     </thead>
     <tbody>
-    {#if isLoading}
-        <tr>
-            <td colspan="99">
-                <CenteredSpinner/>
-            </td>
-        </tr>
-    {:else}
-        {#each allContent as content (content.resourceId)}
-            <tr class="cursor-pointer" onclick={() => toggleSelectedId(content.resourceId)}>
-                <td
-                ><input
-                        type="checkbox"
-                        checked={selectedIds.some((id) => id === content.resourceId)}
-                        class="checkbox"
-                /></td
-                >
-                <td>{content.title}</td>
-                <td class="text-end">{content.wordCount}</td>
+        {#if isLoading}
+            <tr>
+                <td colspan="99">
+                    <CenteredSpinner />
+                </td>
             </tr>
         {:else}
-            {#if hasSearched}
-                <tr>
-                    <td colspan="99">
-                        <div class="text-center">No results</div>
-                    </td>
+            {#each allContent as content (content.resourceId)}
+                <tr class="cursor-pointer" onclick={() => toggleSelectedId(content.resourceId)}>
+                    <td
+                        ><input
+                            type="checkbox"
+                            checked={selectedIds.some((id) => id === content.resourceId)}
+                            class="checkbox"
+                        /></td
+                    >
+                    <td>{content.title}</td>
+                    <td class="text-end">{content.wordCount}</td>
                 </tr>
-            {/if}
-        {/each}
-    {/if}
+            {:else}
+                {#if hasSearched}
+                    <tr>
+                        <td colspan="99">
+                            <div class="text-center">No results</div>
+                        </td>
+                    </tr>
+                {/if}
+            {/each}
+        {/if}
     </tbody>
 </table>
