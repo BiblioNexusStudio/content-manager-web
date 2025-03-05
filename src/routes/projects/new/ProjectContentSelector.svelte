38511313564d4a1dd0e477b28a8a185ba078454c
Jesse Winston <jesse.winston@biblionexus.org>
2025-03-05 12:23:52 -0500

    lint

--- src/routes/projects/new/ProjectContentSelector.svelte
+++ src/routes/projects/new/ProjectContentSelector.svelte
@@ -3,13 +3,13 @@
     import Select from '$lib/components/Select.svelte';
     import ArrowLeftSmall from '$lib/icons/ArrowLeftSmall.svelte';
     import ArrowRightSmall from '$lib/icons/ArrowRightSmall.svelte';
-    import {enterKeyHandler} from '$lib/utils/enter-key-action';
-    import {getFromApi} from '$lib/utils/http-service';
-    import {parseNumbersListFromString} from '$lib/utils/number-list-parser';
-    import {sortByKeys} from '$lib/utils/sorting';
-    import type {PageData} from './$types';
+    import { enterKeyHandler } from '$lib/utils/enter-key-action';
+    import { getFromApi } from '$lib/utils/http-service';
+    import { parseNumbersListFromString } from '$lib/utils/number-list-parser';
+    import { sortByKeys } from '$lib/utils/sorting';
+    import type { PageData } from './$types';
     import ProjectContentSelectorTable from './ProjectContentSelectorTable.svelte';
-    import type {ResourceContentForSelection} from './types';
+    import type { ResourceContentForSelection } from './types';
 
     interface Props {
         data: PageData;
@@ -29,7 +29,7 @@
         isAlreadyTranslated,
     }: Props = $props();
 
-    const {parentResources} = data;
+    const { parentResources } = data;
 
     let bibleBooks = data.bibleBooks;
     let resourceTypeId: string | null = $state(null);
@@ -125,11 +125,11 @@
                 <span class="label-text">Resource</span>
             </div>
             <Select
-                    bind:value={resourceTypeId}
-                    class="select select-bordered"
-                    disabled={disabled || allContentOnRight.length > 0}
-                    isNumber={true}
-                    options={[
+                bind:value={resourceTypeId}
+                class="select select-bordered"
+                disabled={disabled || allContentOnRight.length > 0}
+                isNumber={true}
+                options={[
                     { value: null, label: 'Select Resource' },
                     ...(parentResources || []).map((r) => ({ value: r.id, label: r.displayName })),
                 ]}
@@ -140,10 +140,10 @@
                 <span class="label-text">Book</span>
             </div>
             <Select
-                    bind:value={bookCode}
-                    class="select select-bordered"
-                    disabled={!resourceTypeId}
-                    options={[
+                bind:value={bookCode}
+                class="select select-bordered"
+                disabled={!resourceTypeId}
+                options={[
                     { value: null, label: 'Select Book' },
                     ...(bibleBooks?.map((b) => ({ value: b.code, label: b.localizedName })) ?? []),
                 ]}
@@ -154,11 +154,11 @@
                 <span class="label-text">Chapters</span>
             </div>
             <input
-                    bind:value={chaptersString}
-                    class="input input-bordered"
-                    disabled={!bookCode}
-                    placeholder="all, or 1-5, or 3,7"
-                    use:enterKeyHandler={fetchContent}
+                bind:value={chaptersString}
+                class="input input-bordered"
+                disabled={!bookCode}
+                placeholder="all, or 1-5, or 3,7"
+                use:enterKeyHandler={fetchContent}
             />
         </label>
         <label class="form-control">
@@ -166,26 +166,24 @@
                 <span class="label-text">&nbsp;</span>
             </div>
             <input
-                    bind:value={searchQuery}
-                    class="input input-bordered"
-                    disabled={!resourceTypeId}
-                    placeholder="Search"
-                    use:enterKeyHandler={fetchContent}
+                bind:value={searchQuery}
+                class="input input-bordered"
+                disabled={!resourceTypeId}
+                placeholder="Search"
+                use:enterKeyHandler={fetchContent}
             />
         </label>
         <label class="form-control">
             <div class="label">
                 <span class="label-text">&nbsp;</span>
             </div>
-            <button class="btn btn-primary" disabled={!searchQuery && chapters.length === 0} onclick={fetchContent}
-            >
+            <button class="btn btn-primary" disabled={!searchQuery && chapters.length === 0} onclick={fetchContent}>
                 {#if isFetching}
                     <span class="loading loading-spinner"></span>
                 {:else}
                     Search
                 {/if}
-            </button
-            >
+            </button>
         </label>
     </div>
     <div class="mb-16 flex flex-row space-x-2 overflow-hidden pt-2">
@@ -194,53 +192,48 @@
                 {isForAquiferization
                     ? 'Unaquiferized Content'
                     : isAlreadyTranslated
-                        ? 'Translated Content'
-                        : 'Untranslated Content'}
+                      ? 'Translated Content'
+                      : 'Untranslated Content'}
             </div>
             <div class="overflow-auto">
                 <ProjectContentSelectorTable
-                        allContent={(fetchedContentForLeft ?? []).filter(
+                    allContent={(fetchedContentForLeft ?? []).filter(
                         (c) => !allContentIdsOnRight.some((id) => id === c.resourceId)
                     )}
-                        bind:selectedIds={idsSelectedOnLeft}
-                        hasSearched={fetchedContentForLeft !== null}
-                        isLoading={isFetching}
+                    bind:selectedIds={idsSelectedOnLeft}
+                    hasSearched={fetchedContentForLeft !== null}
+                    isLoading={isFetching}
                 />
             </div>
         </div>
         <div class="flex flex-col space-y-2 pt-6">
             <button
-                    class="btn btn-primary btn-sm"
-                    disabled={idsSelectedOnLeft.length === 0}
-                    onclick={() => moveToRight(false)}>
-                <ArrowRightSmall/>
-            </button
+                class="btn btn-primary btn-sm"
+                disabled={idsSelectedOnLeft.length === 0}
+                onclick={() => moveToRight(false)}
             >
-            <button class="btn btn-primary btn-sm" disabled={idsSelectedOnRight.length === 0} onclick={moveToLeft}
-            >
-                <ArrowLeftSmall/>
-            </button
-            >
+                <ArrowRightSmall />
+            </button>
+            <button class="btn btn-primary btn-sm" disabled={idsSelectedOnRight.length === 0} onclick={moveToLeft}>
+                <ArrowLeftSmall />
+            </button>
         </div>
         <div class="flex flex-1 flex-col">
             <div class="text-md font-semibold">Project Content</div>
             <div class="overflow-auto">
-                <ProjectContentSelectorTable
-                        allContent={allContentOnRight}
-                        bind:selectedIds={idsSelectedOnRight}
-                />
+                <ProjectContentSelectorTable allContent={allContentOnRight} bind:selectedIds={idsSelectedOnRight} />
             </div>
         </div>
     </div>
 </div>
 
 <Modal
-        bind:open={showingAquiferizeEditorReviewModal}
-        description={`The following resource items are not in a Complete status:\n\n${selectedOnLeftBeingAquiferized
+    bind:open={showingAquiferizeEditorReviewModal}
+    description={`The following resource items are not in a Complete status:\n\n${selectedOnLeftBeingAquiferized
         .map((r) => r.title)
         .join('\n')}\n\nIf new items are created, the published version will be used and any changes being made will not
 be reflected. Are you sure you want to add them to this project?`}
-        header="Not in Complete Status"
-        primaryButtonOnClick={() => moveToRight(true)}
-        primaryButtonText="Add Anyway"
+    header="Not in Complete Status"
+    primaryButtonOnClick={() => moveToRight(true)}
+    primaryButtonText="Add Anyway"
 />
