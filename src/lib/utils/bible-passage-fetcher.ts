ccac3e94af0c71120f2564baea834ac0b5ae4447
kaseywright <kasey.wright@biblionexus.org>
2025-03-15 12:21:38 -0400

    Stop filtering bible text upon api response

--- src/lib/utils/bible-passage-fetcher.ts
+++ src/lib/utils/bible-passage-fetcher.ts
@@ -22,15 +22,6 @@ export async function fetchBiblePassages(
 
     for (let i = start.bookId; i <= end.bookId; i++) {
         let bookStart = start;
-        let bookEnd = end;
-
-        if (spansMultipleBooks && (start.bookId === i || (start.bookId !== i && end.bookId !== i))) {
-            bookEnd = {
-                bookId: i,
-                chapter: 999,
-                verse: 999,
-            };
-        }
 
         if (spansMultipleBooks && (end.bookId === i || (start.bookId !== i && end.bookId !== i))) {
             bookStart = {
@@ -40,7 +31,7 @@ export async function fetchBiblePassages(
             };
         }
 
-        const text = await getText(bibleId, bookStart, bookEnd);
+        const text = await getText(bibleId, bookStart);
         if (text) {
             texts.push(text);
         }
@@ -49,16 +40,10 @@ export async function fetchBiblePassages(
     return texts;
 }
 
-const getText = async (bibleId: number, start: Verse, end: Verse): Promise<BibleBookTexts | null> => {
+const getText = async (bibleId: number, start: Verse): Promise<BibleBookTexts | null> => {
     const bibleBook = await fetchBibleBookTexts(bibleId, start.bookId);
-    if (bibleBook) {
-        bibleBook.chapters = bibleBook.chapters.filter((b) => b.number >= start.chapter && b.number <= end.chapter);
-        if (!bibleBook.chapters[0]) {
-            // the Bible is missing expected data, return null rather than blowing up on the lines below
-            return null;
-        }
-        bibleBook.chapters[0].verses = bibleBook.chapters[0].verses.filter((v) => v.number >= start.verse);
-        bibleBook.chapters.at(-1)!.verses = bibleBook.chapters.at(-1)!.verses.filter((v) => v.number <= end.verse);
+    if (bibleBook && bibleBook.chapters.length === 0) {
+        return null;
     }
 
     return bibleBook;
