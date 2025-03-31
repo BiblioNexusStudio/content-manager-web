e2087d7cb5b2b6fa2d56bd31c6168983998f230f
Elisabeth <shakespearee04@gmail.com>
2025-03-31 14:32:31 -0400

    add new route for management reports that has a dropdown populated with dynamic reports with showinDropdown=2

---
+++ src/routes/management-reports/Report.svelte
@@ -0,0 +1,214 @@
+<script lang="ts">
+    import DatePicker from '$lib/components/DatePicker.svelte';
+    import Select from '$lib/components/Select.svelte';
+    import { companiesToIgnore, type Company, type Language, type ParentResource } from '$lib/types/base';
+    import { DynamicReportType, type DynamicReport, type DynamicReportResult } from '$lib/types/reporting';
+    import Icon from 'svelte-awesome';
+    import { refresh } from 'svelte-awesome/icons';
+    import { createListSorter } from '$lib/utils/sorting';
+    import { buildQueryString, searchParameters } from '$lib/utils/sveltekit-search-params';
+    import { getFromApi } from '$lib/utils/http-service';
+    import { page } from '$app/state';
+    import { onMount } from 'svelte';
+    import { isAuthorizationError } from '$lib/utils/http-errors';
+    import { _defaultTableRowsPerPage, _searchParamsConfig } from './+page';
+    import ReportTablePagination from '../reporting/[slug]/ReportTablePagination.svelte';
+    import BarChart from './BarChart.svelte';
+    import LineChart from './LineChart.svelte';
+    import ReportTable from './ReportTable.svelte';
+
+    interface Props {
+        reportData?: DynamicReport | null;
+        companies: Company[];
+        languages: Language[];
+        parentResources: ParentResource[];
+        loading?: boolean;
+        errorMessage?: string | null;
+    }
+
+    let {
+        reportData = $bindable(),
+        companies,
+        languages,
+        parentResources,
+        loading = $bindable(),
+        errorMessage = $bindable(),
+    }: Props = $props();
+
+    const searchParams = searchParameters(_searchParamsConfig, {
+        runLoadAgainWhenParamsChange: false,
+    });
+
+    let startDate = $state($searchParams.startDate);
+    let endDate = $state($searchParams.endDate);
+    let languageId = $state($searchParams.languageId);
+    let parentResourceId = $state($searchParams.parentResourceId);
+    let companyId = $state($searchParams.companyId);
+    let report = $derived($searchParams.report);
+
+    $effect(() => {
+        initializeFromReport(reportData);
+    });
+
+    function initializeFromReport(reportData?: DynamicReport | null) {
+        if (reportData) {
+            if (!startDate) {
+                startDate = reportData.startDate;
+            }
+            if (!endDate) {
+                endDate = reportData.endDate;
+            }
+        }
+    }
+    async function fetchReport() {
+        const queryString = buildQueryString([
+            { key: 'startDate', value: $searchParams.startDate, ignoreIfEquals: '' },
+            { key: 'endDate', value: $searchParams.endDate, ignoreIfEquals: '' },
+            { key: 'languageId', value: $searchParams.languageId, ignoreIfEquals: 0 },
+            { key: 'parentResourceId', value: $searchParams.parentResourceId, ignoreIfEquals: 0 },
+            { key: 'companyId', value: $searchParams.companyId, ignoreIfEquals: 0 },
+        ]);
+
+        try {
+            reportData = await getFromApi<DynamicReport>(
+                `/reports/dynamic/${page.params.slug ?? report}?${queryString}`,
+                fetch
+            );
+        } catch (error) {
+            if (isAuthorizationError(error)) {
+                errorMessage = 'You are not authorized to see this report.';
+            } else {
+                errorMessage = 'An error occurred.';
+            }
+        } finally {
+            loading = false;
+        }
+    }
+
+    function refetch() {
+        loading = true;
+        if (
+            startDate !== $searchParams.startDate ||
+            endDate !== $searchParams.endDate ||
+            languageId !== $searchParams.languageId ||
+            parentResourceId !== $searchParams.parentResourceId ||
+            companyId !== $searchParams.companyId
+        ) {
+            $searchParams.paginationStart = 0;
+            $searchParams.paginationEnd = _defaultTableRowsPerPage;
+        }
+
+        $searchParams.startDate = startDate ?? '';
+        $searchParams.endDate = endDate ?? '';
+        $searchParams.languageId = languageId;
+        $searchParams.parentResourceId = parentResourceId;
+        $searchParams.companyId = companyId;
+
+        fetchReport();
+    }
+
+    let sortReportTable = $derived(
+        reportData &&
+            createListSorter<DynamicReportResult>(
+                Object.fromEntries(reportData.columns.map((name, index) => [name, { primarySortKeys: [index] }]))
+            )
+    );
+
+    let sortedResults = $derived(
+        reportData && sortReportTable && sortReportTable(reportData.results, $searchParams.sort)
+    );
+
+    onMount(() => {
+        fetchReport();
+    });
+</script>
+
+{#if reportData}
+    <div class="flex max-h-full max-h-screen flex-shrink flex-col space-y-4 p-4">
+        <div class="flex items-center justify-between">
+            <h1 class="text-3xl capitalize">{reportData.name}</h1>
+            {#if reportData.type === DynamicReportType.Table && reportData.results.length > _defaultTableRowsPerPage}
+                <ReportTablePagination
+                    bind:paginationStart={$searchParams.paginationStart}
+                    bind:paginationEnd={$searchParams.paginationEnd}
+                    perPage={_defaultTableRowsPerPage}
+                    totalCount={reportData.results.length}
+                />
+            {/if}
+        </div>
+        <div class="flex flex-row space-x-6">
+            {#if reportData.acceptsLanguage}
+                <Select
+                    bind:value={languageId}
+                    isNumber={true}
+                    class="select select-bordered min-w-[10rem] flex-shrink"
+                    options={[
+                        { value: 0, label: 'All Languages' },
+                        ...languages.map((l) => ({ value: l.id, label: l.englishDisplay })),
+                    ]}
+                />
+            {/if}
+            {#if reportData.acceptsParentResource}
+                <Select
+                    bind:value={parentResourceId}
+                    isNumber={true}
+                    class="select select-bordered min-w-[10rem] flex-shrink"
+                    options={[
+                        { value: 0, label: 'All Resources' },
+                        ...parentResources.map((t) => ({ value: t.id, label: t.displayName })),
+                    ]}
+                />
+            {/if}
+            {#if reportData.acceptsCompany}
+                <Select
+                    bind:value={companyId}
+                    isNumber={true}
+                    class="select select-bordered min-w-[10rem] flex-shrink"
+                    options={[
+                        { value: 0, label: 'Select Company' },
+                        ...(companies
+                            ?.filter((c) => !companiesToIgnore.includes(c.name))
+                            ?.map((c) => ({ value: c.id, label: c.name })) ?? []),
+                    ]}
+                />
+            {/if}
+            {#if reportData.acceptsDateRange}
+                <div class="flex flex-row items-center space-x-2">
+                    <span>Date Range: </span>
+                    <DatePicker bind:date={startDate} latestDate={endDate} />
+                    <span>-</span>
+                    <DatePicker bind:date={endDate} earliestDate={startDate} />
+                </div>
+            {/if}
+            {#if reportData.acceptsDateRange || reportData.acceptsLanguage || reportData.acceptsParentResource || reportData.acceptsCompany}
+                <button class="btn btn-link mx-1!" onclick={refetch}>
+                    <Icon data={refresh} />
+                </button>
+            {/if}
+        </div>
+        {#if reportData.type === DynamicReportType.BarChart}
+            <div class="relative ms-5 me-10 h-full flex-shrink overflow-hidden">
+                <BarChart report={reportData} />
+            </div>
+        {:else if reportData.type === DynamicReportType.LineChart}
+            <div class="relative ms-5 me-10 h-full flex-shrink overflow-hidden">
+                <LineChart report={reportData} />
+            </div>
+        {:else if reportData.type === DynamicReportType.Table && sortedResults}
+            <div>
+                <div class="relative flex-shrink overflow-hidden">
+                    <ReportTable
+                        bind:searchParams={$searchParams}
+                        report={reportData}
+                        sortedAndPaginatedResults={sortedResults.slice(
+                            $searchParams.paginationStart,
+                            $searchParams.paginationEnd
+                        )}
+                    />
+                </div>
+            </div>
+        {:else}
+            <p>Unsupported report type</p>
+        {/if}
+    </div>
+{/if}
---
+++ src/routes/management-reports/ReportTablePagination.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+    import chevronLeft from 'svelte-awesome/icons/chevronLeft';
+    import chevronRight from 'svelte-awesome/icons/chevronRight';
+    import Icon from 'svelte-awesome/components/Icon.svelte';
+
+    interface Props {
+        perPage: number;
+        paginationStart: number;
+        paginationEnd: number;
+        totalCount: number;
+    }
+
+    let { perPage, paginationStart = $bindable(), paginationEnd = $bindable(), totalCount }: Props = $props();
+
+    function handleLeftClick() {
+        if (paginationStart === 0) return;
+
+        if (paginationEnd === totalCount) {
+            const remainder = paginationEnd % perPage;
+
+            paginationEnd = paginationEnd - remainder;
+            paginationStart = paginationStart - perPage;
+            return;
+        }
+
+        paginationStart = paginationStart - perPage;
+        paginationEnd = paginationEnd - perPage;
+
+        if (paginationStart < perPage) {
+            paginationStart = 0;
+            paginationEnd = perPage;
+        }
+    }
+
+    function handleRightClick() {
+        if (paginationEnd === totalCount) return;
+        if (paginationEnd + perPage > totalCount) {
+            paginationStart = paginationStart + perPage;
+            paginationEnd = totalCount;
+            return;
+        }
+        paginationStart = paginationStart + perPage;
+        paginationEnd = paginationEnd + perPage;
+    }
+</script>
+
+<div class="flex h-full w-auto items-center justify-between space-x-2">
+    <button class="btn btn-link disabled:btn-ghost" onclick={handleLeftClick} disabled={paginationStart === 0}>
+        <Icon data={chevronLeft} />
+    </button>
+
+    <span>{paginationStart + 1} - {paginationEnd} of {totalCount}</span>
+
+    <button class="btn btn-link disabled:btn-ghost" onclick={handleRightClick} disabled={paginationEnd === totalCount}>
+        <Icon data={chevronRight} />
+    </button>
+</div>
